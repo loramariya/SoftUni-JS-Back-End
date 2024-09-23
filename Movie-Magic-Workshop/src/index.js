5945cd7397ebdb9ae6e01489b1c3e1b1c62f2f16
loramariya <loramaria.raynova@gmail.com>
2024-09-23 13:29:14 +0300

    Setup express

--- Movie-Magic-Workshop/src/index.js
+++ Movie-Magic-Workshop/src/index.js
@@ -1,1 +1,9 @@
 import express from 'express';
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('it works!');
+})
+
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
