936008f611636f0c03e65a69efc2989e39d4a65b
loramariya <loramaria.raynova@gmail.com>
2024-10-15 14:08:32 +0300

    Handle invalid token scenario

--- Movie-Magic-Workshop/src/middlewares/authMiddleware.js
+++ Movie-Magic-Workshop/src/middlewares/authMiddleware.js
@@ -14,7 +14,9 @@ export const authMiddleware = (req, res, next) => {
         
        return next();
     } catch (error) {
-        
+        res.clearCookie('auth');
+
+        res.redirect('/auth/login');
     }
 
 };
